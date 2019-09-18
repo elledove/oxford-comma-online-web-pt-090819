e46d952049634fb58557eae1b468f28abc99a3ff
Lauren Dove <elledoveinc@gmail.com>
2019-09-18 21:21:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+ if array.length == 1 
+   array[0]
+   
+   elsif
+   array.length == 2 
+   array.join (" and ")
+   
+   elsif 
+   array.length >= 3
+   new_array = "and #{array[-1]}"
+   array.pop
+  array.push(new_array)
+  return array.join(", ")
+  end
 end
