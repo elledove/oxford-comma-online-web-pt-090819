11861903a160cb3f563a99c8c04c2e593be31c49
Lauren Dove <elledoveinc@gmail.com>
2019-09-18 20:56:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,10 +5,5 @@ def oxford_comma(array)
    elsif
    array.length == 2 
    array.join (" and ")
-   elsif 
-   array.length >= 3
-   new_array = "and #{array[-1]}" array.pop
-  array << new_array
    
-   end
-end
+   
